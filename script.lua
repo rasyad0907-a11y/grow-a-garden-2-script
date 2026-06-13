940e73be9ed4a3fc08c76b1881f5b58ae5d165b3
rasyad0907-a11y <rasyad0907@gmail.com>
2026-06-13 23:42:03 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,4 +1,28 @@
-local humanoid = script.Parent:WaitForChild("Humanoid")
+local player = game.Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local humanoid = character:WaitForChild("Humanoid")
 
-humanoid.WalkSpeed = 24
-humanoid.JumpPower = 70
+local infinityJump = false
+
+-- tombol ON/OFF (tekan J)
+game:GetService("UserInputService").InputBegan:Connect(function(input, gameProcessed)
+	if gameProcessed then return end
+
+	if input.KeyCode == Enum.KeyCode.J then
+		infinityJump = not infinityJump
+
+		if infinityJump then
+			humanoid.JumpPower = 200 -- super tinggi
+			humanoid.UseJumpPower = true
+			print("Super Jump ON")
+		else
+			humanoid.JumpPower = 50 -- normal
+			print("Super Jump OFF")
+		end
+	end
+end)
+
+-- biar tetap aktif kalau respawn
+player.CharacterAdded:Connect(function(char)
+	char:WaitForChild("Humanoid").JumpPower = infinityJump and 200 or 50
+end)
